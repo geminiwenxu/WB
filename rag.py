b2f71ff8efab65c33c098c4c090905665b5b64ce
Wenxu <geminiwenxu@gmail.com>
2025-08-05 12:11:57 +0200

    initial commit

---
+++ rag.py
@@ -0,0 +1,52 @@
+import os
+import signal
+import sys 
+import google.generativeai as genai
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import HuggingFaceEmbeddings
+GEMINI_API_KEY = '7d00a1fa19974c7c1677698d59b280b5ae013196'
+
+def signal_handler(sig, frame):
+    print('\nThanks for using gemini')
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+def generate_rag_prompt(query, context):
+    """"
+    To generate promot based on the query from user and relevant context based on similarity 
+    """
+    escaped = context.replace("'", "").replace("\n", "")
+    prompt = ("""you are a bot to retrieve information from our RAG system and answer the questions.
+              question: '{query}'
+              context: '{context}'
+              answer: 
+                """).format(query=query, context=context)
+    return prompt
+
+
+def get_relevant_context_from_db(query):
+    context = ""
+    embedding_function = HuggingFaceEmbeddings(model_name= 'sentence-transformers/all-MiniLM-L6-v2', model_kwargs={"device":"cpu"})
+    vector_db = Chroma(persist_directory='chroma_db_pdf', embedding_function=embedding_function)
+    search_results = vector_db.similarity_search(query, k=6) # Chroma distance is the L2 norm squared; k is the amount of documents to return
+    for result in search_results:
+        context += result.page_content + "\n"
+    return context
+
+
+def generate_answer(prompt):
+    genai.configure(api_key=GEMINI_API_KEY)
+    model =genai.GenerativeModel(model_name='gemini-pro')
+    answer = model.generate_content(prompt)
+    return answer.text
+
+
+while True:
+    print('----------------')
+    print('what is your question?')
+    query = input('question: ')
+    context = get_relevant_context_from_db(query)
+    prompt = generate_rag_prompt(query=query, context=context)
+    answer = generate_answer(prompt=prompt)
+    print(answer)
