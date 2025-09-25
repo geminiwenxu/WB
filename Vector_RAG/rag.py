5b99a8649633353872fdb963df6220e9766ca18f
Wenxu <geminiwenxu@gmail.com>
2025-09-25 12:33:01 +0200

    add meta data

--- Vector_RAG/rag.py
+++ Vector_RAG/rag.py
@@ -3,22 +3,46 @@ import google.generativeai as genai
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from model import VertexAIReader
+from sentence_transformers import SentenceTransformer
+import torch
+
+def get_relevant_context_from_db(query):
+    context = ""
+    #embedding_function = HuggingFaceEmbeddings(model_name= 'sentence-transformers/all-MiniLM-L6-v2',model_kwargs={"device": "cpu"}, encode_kwargs={"normalize_embeddings": False})
+    
 
 
+    from langchain_community.embeddings import HuggingFaceEmbeddings
+
+    embedding_function = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2",
+    model_kwargs={"device": "cpu"},
+    encode_kwargs={"normalize_embeddings": False}
+)
+
+# Force reload underlying model to CPU only
+    embedding_function.client = SentenceTransformer(
+    "sentence-transformers/all-MiniLM-L6-v2", device="cpu"
+)
 
 
+    vector_db = Chroma(persist_directory='chroma_db_pdf', embedding_function=embedding_function)
+    search_results = vector_db.similarity_search(query, k=2) # Chroma distance is the L2 norm squared; k is the amount of documents to return
+    print("----------------------------")
+    #print(search_results)
+    for result in search_results:
+        print(type(result))
+        print("result",len(result.metadata))
+        context += result.metadata['source']+str(result.metadata['page'])+result.metadata["UPLOAD_DATE"]+result.page_content + "\n"
+        print("context",context)
+    return context
 
-def signal_handler(sig, frame):
-    print('\nThanks for using gemini')
-    sys.exit(0)
-
-signal.signal(signal.SIGINT, signal_handler)
 
 def generate_rag_prompt(query, context):
     """"
     To generate promot based on the query from user and relevant context based on similarity 
     """
-    escaped = context.replace("'", "").replace("\n", "")
+    context = context.replace("'", "").replace("\n", "")
     prompt = ("""you are a bot to retrieve information from our RAG system and answer the questions.
               question: '{query}'
               context: '{context}'
@@ -26,25 +50,12 @@ def generate_rag_prompt(query, context):
                 """).format(query=query, context=context)
     return prompt
 
-
-def get_relevant_context_from_db(query):
-    context = ""
-    embedding_function = HuggingFaceEmbeddings(model_name= 'sentence-transformers/all-MiniLM-L6-v2', model_kwargs={"device":"cpu"})
-    vector_db = Chroma(persist_directory='chroma_db_pdf', embedding_function=embedding_function)
-    search_results = vector_db.similarity_search(query, k=6) # Chroma distance is the L2 norm squared; k is the amount of documents to return
-    for result in search_results:
-        context += result.page_content + "\n"
-    return context
-
-
-while True:
-    print('----------------')
-    print('what is your question?')
-    query = input('question: ')
+def RAG(option, country):
+    query = "hello world" + option + country 
     context = get_relevant_context_from_db(query)
-    print("context:", context)
     prompt = generate_rag_prompt(query=query, context=context)
-    print("prompt", prompt)
     generator = VertexAIReader()
     result = generator.generate_content(prompt)
-    print(result)
+    return context, result
+
+RAG("HELLO", "WORLD")
